42c5da50dac145271cd814f6b00cc2be45bf9110
AmonAmmar <111253891+AmonAmmar@users.noreply.github.com>
2022-08-15 06:44:01 +0700

    Add files via upload

---
+++ player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour 
+{
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidbodycomponent;
+
+	// Use this for initialization
+	void Start () 
+    {
+        rigidbodycomponent = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+	}
+
+    private void FixedUpdate()
+    {
+        rigidbodycomponent.velocity = new Vector3(horizontalInput, rigidbodycomponent.velocity.y, 0);
+        
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+        {
+            return;
+        }
+
+        if (jumpKeyWasPressed)
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(horizontalInput, 0, 0);
+            rigidbodycomponent.AddForce(Vector3.up * 7, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+    } 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
